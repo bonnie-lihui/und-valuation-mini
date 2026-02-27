4d25b09be0fa08a0f9f97a8705e742389447d032
li.hui_wb5015 <li.hui_wb5015@immomo.com>
2026-02-27 15:50:30 +0800

    fix: 更新后端接口基础地址
    - 将基础接口地址从 https://fund.baoyisi.com 更新为 https://fund.solaboom.cn，以确保正确的请求配置。

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 /**
  * 后端接口基础地址
- * - 线上：https://fund.baoyisi.com（需在小程序后台配置 request 合法域名）
+ * - 线上：https://fund.solaboom.cn（需在小程序后台配置 request 合法域名）
  * - 开发/真机调试：可改为 http://localhost:5599 或 http://实际IP:5599
  */
-const BASE_URL = 'https://fund.baoyisi.com';
+const BASE_URL = 'https://fund.solaboom.cn';
 
 module.exports = {
   BASE_URL,
